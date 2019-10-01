355e9553caa05db91c3328915701375d32ffe329
nonac <nonacyang@gmail.com>
2019-10-01 22:05:22 +0100

    modified:   week_1/a.out 	new file:   week_2/Excercise2_1.c 	new file:   week_2/Excercise2_2.c 	new file:   week_2/a.out 	deleted:    week_1/.Excercise4_12.c.swp

---
+++ week_2/Excercise2_1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+void Createvowel(char *vowel);
+void Createunvowel(char *unvowel,char *vowel);
+
+int main()
+{
+	char unvowel[21],vowel[5]={'a','e','i','o','u'};;
+	Createunvowel(unvowel,vowel);	
+	int i,j,k,cnt=1;
+	for(i=0;i<21;i++)
+	{
+		for(j=0;j<5;j++)
+		{
+			for(k=0;k<21;k++)
+			{
+				printf("%d %c%c%c\n",cnt,unvowel[i],vowel[j],unvowel[k]);
+				cnt++;
+			}
+		}
+	}		
+		
+}
+
+void Createunvowel(char *unvowel,char *vowel)
+{
+	int i,j=0,k=0;
+	char temp;
+	for(i=0;i<26;i++)
+	{
+		temp='a'+i;
+		if(temp!=vowel[j])
+		{
+			unvowel[k]=temp;
+			k++;
+		}
+		else
+		{
+			j++;
+		}
+	}
+}	
---
+++ week_2/Excercise2_2.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<math.h>
+
+void Createletter(char *letter);
+void Createunvowel(char *unvowel,char *vowel);
+int Sub(char c);
+int Isprime(int k);
+
+int main()
+{
+        char unvowel[21],vowel[5]={'a','e','i','o','u'};
+	char letter[26];
+	Createletter(letter);
+        Createunvowel(unvowel,vowel);
+        int i,j,k,cnt=1;
+        for(i=0;i<26;i++)
+        {
+                for(j=0;j<5;j++)
+                {
+			if(Isprime(Sub(letter[i])*2+Sub(vowel[j]))&&(letter[i]!=vowel[j]))
+			{
+				printf("%d %c%c%c\n",cnt,letter[i],vowel[j],letter[i]);
+				cnt++;
+			}		
+                }
+        }
+
+}
+
+void Createletter(char *letter)
+{
+	int i;
+	for(i=0;i<26;i++)
+	{
+		letter[i]='a'+i;
+	}
+}
+
+void Createunvowel(char *unvowel,char *vowel)
+{
+        int i,j=0,k=0;
+        char temp;
+        for(i=0;i<26;i++)
+        {
+                temp='a'+i;
+                if(temp!=vowel[j])
+                {
+                        unvowel[k]=temp;
+                        k++;
+                }
+                else
+                {
+                        j++;
+                }
+        }
+}
+
+int Sub(char c)
+{
+	return (c-'a'+1);
+}
+
+int Isprime(int k)
+{
+	int temp;
+	for(temp=2;temp<k;temp++)
+	{
+		if(k%temp==0) return 0;
+	}
+	return 1;
+}
