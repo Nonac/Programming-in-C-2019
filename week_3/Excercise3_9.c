2dea55917205f624caa0b7c8140e43a52ddc39cf
nonac <nonacyang@gmail.com>
2019-10-08 20:21:14 +0100

    modified:   week_3/Excercise3_9.c 	modified:   week_3/a.out

--- week_3/Excercise3_9.c
+++ week_3/Excercise3_9.c
@@ -1,112 +1,97 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#define STORELEN 4
 #define _tolower(c) ((c)+'a'-'A')
 #define _toupper(c) ((c)+'A'-'a')
 
-void Soundex(char *sp,char *resp);
+void Soundex(char *sp,char *resp,int len);
+void Search(char *sp,char *target,int position,char mark);
+
 
 int main()
 {
-	char res[STORELEN]={0},*resp=res;
-	char *sp=(char *)malloc(sizeof(char));
-	char str;
-	int i,len=0;
-	printf("Please enter the name:\n");
-	
-	while((str=getchar())!='\n' )
-	{
-		sp[len]=str;
-		sp=(char *)realloc(sp,(++len+1)*sizeof(char));	
-	}
-	
-	Soundex(sp,resp);	
-	printf("%s",resp);
+    char *resp;
+    char *sp=(char *)malloc(sizeof(char));
+    char str;
+    int i,len=0;
+    printf("Please enter the name:\n");
+
+    while((str=getchar())!='\n' )
+    {
+        sp[len]=((str<='z')&&(str>='a'))?str:_tolower(str);
+        sp=(char *)realloc(sp,(++len+1)*sizeof(char));
+    }
+    resp=(char *)malloc(len* sizeof(char));
+    Soundex(sp,resp,len);
+    resp[0]=_toupper(resp[0]);
+    resp[4]='\0';
+    printf("%s",resp);
 }
 
-void Soundex(char *sp,char *resp)
+void Soundex(char *sp,char *resp,int len)
 {
-	int k=0,i=1,j=1,splen=strlen(sp);
-	char _LABIAL[]={'b','f','p','v'};
-	char _GUTTERALS_SIBILANTS[]={'c','g','j','k','q','s','x','z'};
-	char _DENTAL[]={'d','t'};
-	char _LONG_LIQUID[]={'l'};
-	char _NASAL[]={'m','n'};
-	char _SHORT_LIQUID[]={'r'};
-	char _SKIPPED[]={'a','e','h','i','o','u','w','y'};
+    int i,j,k,splen= len;
+    char first;
+    char _LABIAL[]={'b','f','v','p','\0'};
+    char _GUTTERALS_SIBILANTS[]={'c','g','j','k','q','s','x','z','\0'};
+    char _DENTAL[]={'d','t','\0'};
+    char _LONG_LIQUID[]={'l','\0'};
+    char _NASAL[]={'m','n','\0'};
+    char _SHORT_LIQUID[]={'r','\0'};
+    char _SKIPPED[]={'a','e','h','i','o','u','w','y','\0'};
 
-	resp[0]=sp[0];
-	
-	while((i<4)&&(j<splen))
-	{
-		while(k<(sizeof(_LABIAL)/sizeof(char)))
-		{
-			if(sp[j]==_LABIAL[k]&&resp[i-1]!='1') 
-			{
-				resp[i++]='1';
-				j++;
-			}else if(resp[i-1]=='1') j++;
-			k++;
-		}
-		k=0;
-		while(k<(sizeof(_GUTTERALS_SIBILANTS)/sizeof(char)))
-		{
-			if(sp[j]==_GUTTERALS_SIBILANTS[k]&&resp[i-1]!='2')
-			{
-				resp[i++]='2';
-				j++;
-			} else if(resp[i-1]=='2') j++;
-		}
-		k=0;	
-		while(k<(sizeof(_DENTAL)/sizeof(char)))
-		{
-			if(sp[j]==_DENTAL[k]&&resp[i-1]!='3') 
-			{
-				resp[i++]='3';
-				j++;
-			}else if(resp[i-1]=='3') j++;
-			k++;
-		}
-		k=0;
-		while(k<(sizeof(_LONG_LIQUID)/sizeof(char)))
-		{
-			if(sp[j]==_LONG_LIQUID[k]&&resp[i-1]!='4') 
-			{
-				resp[i++]='4';
-				j++;
-			}else if(resp[i-1]=='4') j++;
-			k++;
-		}
-		k=0;
-		while(k<(sizeof(_NASAL)/sizeof(char)))
-		{
-			if(sp[j]==_NASAL[k]&&resp[i-1]!='5') 
-			{
-				resp[i++]='5';
-				j++;
-			}else if(resp[i-1]=='5') j++;
-			k++;
-		}
-		k=0;
-		while(k<(sizeof(_SHORT_LIQUID)/sizeof(char)))
-		{
-			if(sp[j]==_SHORT_LIQUID[k]&&resp[i-1]!='6') 
-			{
-				resp[i++]='6';
-				j++;
-			}else if(resp[i-1]=='6') j++;
-			k++;
-		}
-		k=0;
-		while(k<(sizeof(_SKIPPED)/sizeof(char)))
-		{
-			if(sp[j]==_SKIPPED[k]) 
-			{
-				j++;
-			}
-			k++;
-		}
-		k=0;	
-	}
+    first=sp[0];
+
+    for(i=0;i<splen;i++)
+    {
+        Search(sp,_LABIAL,i,'1');
+        Search(sp,_GUTTERALS_SIBILANTS,i,'2');
+        Search(sp,_DENTAL,i,'3');
+        Search(sp,_LONG_LIQUID,i,'4');
+        Search(sp,_NASAL,i,'5');
+        Search(sp,_SHORT_LIQUID,i,'6');
+        Search(sp,_SKIPPED,i,'7');
+    }
+
+    resp[0]=sp[0];
+    /* remove repeat char */
+    j=0;
+    for(i=1;(i<splen);i++)
+    {
+        if((sp[i]!=resp[j]))
+        {
+            resp[++j]=sp[i];
+        }
+    }
+    /* remove skipped char */
+    for(i=1;i<splen;i++)
+    {
+        if(resp[i]=='7')
+        {
+            for(k=i;k<splen;k++)
+            {
+                resp[k]=resp[k+1];
+            }
+            splen--;
+        }
+    }
+    /* fill in with zero */
+    if(j<3)
+    {
+        for(;j<4;j++)
+        {
+            resp[j]='0';
+        }
+    }
+
+    resp[0]=first;
+}
+
+void Search(char *sp,char *target,int position,char mark)
+{
+    int j;
+    for(j=0;j<strlen(target);j++)
+    {
+        if (sp[position] == target[j]) sp[position]=mark;
+    }
 }
