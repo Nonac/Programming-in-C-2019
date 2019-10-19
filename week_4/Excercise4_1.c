1c8233077f3dded83237e5a76024e2763a48fc62
nonac <nonacyang@gmail.com>
2019-10-19 18:06:18 +0100

    modified:   week_4/Excercise4_1.c 	new file:   week_4/a.out 	new file:   week_4/core.22239 	new file:   week_4/wirewcircuit1.txt 	new file:   week_4/wirewcircuit2.txt 	new file:   week_4/wirewinvalid.txt 	new file:   week_4/wireworld_out.txt

--- week_4/Excercise4_1.c
+++ week_4/Excercise4_1.c
@@ -1,33 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define LENGTH 40
-#define EMPTY 0
-#define HEAD 1
-#define TAIL 2
-#define COPPER 3
+#define EMPTY ' '
+#define HEAD 'H'
+#define TAIL 't'
+#define COPPER 'c'
 #define GENERATION 1000
 
-int **create(int **array);
+int **create();
 int **readin(int **array,char *argv);
 int **nextarray(int **array,int **next);
 int **change(int **array,int **next);
 int nextelem(int **array,int arrayx,int arrayy);
 int findhead(int **array,int arrayx,int arrayy);
-int elemtoint(char elem);
 void printout(int **next,FILE *ofp);
-char inttoelem(int elem);
 
 int main(int argc,char **argv)
 {
-    int **array,**next;
+    int **array=create(),**next=create();
     int i;
     FILE *ofp;
     if( argc==2 )
     {
-        ofp=fopen("wireworld_out.txt","r");
-        array=create(array);
-        next=create(next);
-
+        ofp=fopen("wireworld_out.txt","wt");
         array=readin(array,argv[1]);
         for (i = 0; i < GENERATION; ++i) {
             next=nextarray(array,next);
@@ -43,10 +38,10 @@ int main(int argc,char **argv)
     return 0;
 }
 
-int **create(int **array)
+int **create()
 {
     int i;
-    array=(int**)malloc(sizeof(int*)*LENGTH);
+    int **array=(int**)malloc(sizeof(int*)*LENGTH);
     for(i=0;i<LENGTH;i++)
     {
         *(array+i)=(int*)malloc(sizeof(int)*LENGTH);
@@ -58,63 +53,32 @@ int **readin(int **array,char *argv)
 {
     FILE *fp;
     int arrayx,arrayy;
-    char elem;
+    char temp[LENGTH+2];
     fp=fopen(argv,"r");
     for(arrayx=0;arrayx<LENGTH;arrayx++)
     {
-        for(arrayy=0;arrayy<LENGTH;arrayy++) {
-            fscanf(fp,"%c",&elem);
-            *(*(array+arrayx)+arrayy)=elemtoint(elem);
+        fgets(temp,LENGTH+2,fp);
+        for(arrayy=0;arrayy<LENGTH;arrayy++)
+        {
+            *(*(array+arrayx)+arrayy)=temp[arrayy];
         }
-        fscanf(fp,"\n");
+        printf("\n");
     }
     fclose(fp);
     return array;
-}
-
-int elemtoint(char elem)
-{
-    switch (elem){
-        case ' ':
-            return EMPTY;
-        case 'H':
-            return HEAD;
-        case 't':
-            return TAIL;
-        case 'c':
-            return COPPER;
-        default:
-            return -1;
-    }
 }
 
 void printout(int **next,FILE *ofp)
 {
     int nextx,nexty;
     char elem;
-    for(nextx=0;nextx<LENGTH;nextx)
+    for(nextx=0;nextx<LENGTH;nextx++)
     {
         for(nexty=0;nexty<LENGTH;nexty++) {
-            elem=inttoelem(*(*(next+nextx)+nexty));
+            elem=*(*(next+nextx)+nexty);
             fprintf(ofp,"%c",elem);
         }
         fprintf(ofp,"\n");
-    }
-}
-
-char inttoelem(int elem)
-{
-    switch (elem){
-        case EMPTY:
-            return ' ';
-        case HEAD:
-            return 'H';
-        case TAIL:
-            return 't';
-        case COPPER:
-            return 'c';
-        default:
-            return -1;
     }
 }
 
