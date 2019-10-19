9dc5d33e66a85b957d17d2f61b4d961ed65f57fe
nonac <nonacyang@gmail.com>
2019-10-19 11:23:30 +0100

    modified:   week_3/Excercise3_8.c 	modified:   week_4/Excercise4_1.c

--- week_3/Excercise3_8.c
+++ week_3/Excercise3_8.c
@@ -52,64 +52,7 @@ int romanToArabic(char *roman) {
 				else res += C;
 				break;
 		}
-		i++;1000
-
-int romanToArabic(char *roman );
-void test();
-
-int main(int argc, char **argv)
-{
-	test();
-	if( argc==2 )
-	{
-		printf("The roman numeral %s is equal to %d\n", \
-		argv[1], romanToArabic(argv[1]));
-	}
-	else
-	{
-		printf("ERROR: Incorrect usage, try e.g. %s XXI\n", argv[0]);
-	}
-	return 0;
-}
-
-/*Only IV IX XL XC CD CM represant a special subtraction.*/
-
-int romanToArabic(char *roman) {
-	int i=0,res = 0;
-	while(roman[i]!='\0')
-	{
-		switch (roman[i])
-		{
-			case 'V':res += V; break;
-			case 'L':res += L; break;
-			case 'D':res += D; break;
-			case 'M':res += M; break;
-			case 'I':
-				if ((roman[i + 1] == 'V') || roman[i + 1] == 'X') res -= I;
-				else res += I;
-				break;
-			case 'X':
-				if ((roman[i + 1] == 'L') || (roman[i + 1] == 'C')) res -= X;
-				else res += X;
-				break;
-			case 'C':
-				if ((roman[i + 1] == 'D') || (roman[i + 1] == 'M')) res -= C;
-				else res += C;
-				break;
-		}
 		i++;
-	}
-	return res;
-}
-
-/*test three example on handout*/
-void test()
-{
-	assert(romanToArabic("MCMXCIX")==1999);
-	assert(romanToArabic("MCMLXVII")==1967);
-	assert(romanToArabic("MCDXCI")==1491);
-}
-
 	}
 	return res;
 }
--- week_4/Excercise4_1.c
+++ week_4/Excercise4_1.c
@@ -1,63 +1,102 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define LENGTH 40
+#define EMPTY 0
+#define HEAD 1
+#define TAIL 2
+#define COPPER 3
 
+int **create(int **array);
 int **readin(int **array);
-int **create();
-int nextarray(int **array,int **next);
+int **nextarray(int **array,int **next);
 int nextelem(int **array,int arrayx,int arrayy);
+int findhead(int **array,int arrayx,int arrayy);
+
 int main()
 {
-	int **array,**next;
-	array=create();
-	next=create();
+    int **array,**next;
+    array=create(array);
+    next=create(next);
 
-	array=readin(array);
+    array=readin(array);
+    next=nextarray(array,next);
+
 }
 
-int **create(int **arrat)
+int **create(int **array)
 {
-	int i;
-	array=(int**)malloc(sizeof(int*)*40);
-	for(i=0;i<40;i++)
-	{
-		*(array+i)=(int*)malloc(sizeof(int)*40);
-	}		
-	return array;
+    int i;
+    array=(int**)malloc(sizeof(int*)*LENGTH);
+    for(i=0;i<LENGTH;i++)
+    {
+        *(array+i)=(int*)malloc(sizeof(int)*LENGTH);
+    }
+    return array;
 }
 
-int readin(**array)
+int **readin(**array)
 {
-	FILE *fp;
-	fp=fopen();
+    FILE *fp;
+    fp=fopen();
 
-	fclose(fp);
-	return array;
+    fclose(fp);
+    return array;
 }
 
-int nextarray(int **array,int **next)
+int **nextarray(int **array,int **next)
 {
-	int arrayx,arrayy,nextx,nexty;
-	for(arrayx=0;arrayx<40;arrayx++)
-	{
-		for(arrayy=0;arrayy<40;arrayy++)
-		{
-			for(nextx=0;nextx<40;nextx)
-			{
-				for(nexty=0;nexty<40;nexty++)
-				{
-					*(*(next+nextx)+nexty)=nextelem(array,arrayx,arrayy);	
-				}
-			}
-		}
-	}	
+    int arrayx,arrayy,nextx,nexty;
+    for(arrayx=0;arrayx<LENGTH;arrayx++)
+    {
+        for(arrayy=0;arrayy<LENGTH;arrayy++)
+        {
+            for(nextx=0;nextx<LENGTH;nextx)
+            {
+                for(nexty=0;nexty<LENGTH;nexty++)
+                {
+                    *(*(next+nextx)+nexty)=nextelem(array,arrayx,arrayy);
+                }
+            }
+        }
+    }
+    return next;
 }
 
-int nextelem(int **arrayy,int arrayx,int arrayy)
+int nextelem(int **array,int arrayx,int arrayy)
 {
-	switch (*(*(array+arrayx)+arrayy)):
-		case 0:return 0;
-		case 1:return 2;
-		case 2:return 3;
-		case 3:
-				
+    switch (*(*(array+arrayx)+arrayy)) {
+        case EMPTY:
+            return EMPTY;
+        case HEAD:
+            return TAIL;
+        case TAIL:
+            return COPPER;
+        case COPPER:
+            if (findhead(array,arrayx,arrayy))
+            {
+                return HEAD;
+            }else
+            {
+                return COPPER;
+            }
+        default:
+            return -1;
+    }
+}
+
+int findhead(int **array,int arrayx,int arrayy)
+{
+    int i,j,cnt=0;
+    for(i=arrayx-1;i<arrayx+1;i++)
+    {
+        for (j =arrayy-1;j<arrayy+1; j++)
+        {
+            if (!(i<0 || j<0 || i>=LENGTH || j>=LENGTH || (i==arrayx && j==arrayy)))
+            {
+                if(*(*(array+i)+j)==HEAD) cnt++;
+            }
+        }
+    }
+    if((cnt==1)||(cnt==2)) return 1;
+    else return 0;
 }
