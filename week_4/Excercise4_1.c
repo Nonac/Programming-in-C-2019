e24af54ee8a49001f1ecaad38e9524533ad3ca1a
nonac <nonacyang@gmail.com>
2019-10-20 05:04:39 +0100

    modified:   week_4/Excercise4_1.c 	modified:   week_4/a.out 	modified:   week_4/wireworld_out.txt

--- week_4/Excercise4_1.c
+++ week_4/Excercise4_1.c
@@ -6,6 +6,7 @@
 #define TAIL 't'
 #define COPPER 'c'
 #define GENERATION 1000
+#define LINEBREAKLENTH 2
 
 int **create();
 int **readin(int **array,char *argv);
@@ -17,132 +18,133 @@ void printout(int **next,FILE *ofp);
 
 int main(int argc,char **argv)
 {
-    int **array=create(),**next=create();
-    int i;
-    FILE *ofp;
-    if( argc==2 )
-    {
-        ofp=fopen("wireworld_out.txt","wt");
-        array=readin(array,argv[1]);
-        for (i = 0; i < GENERATION; ++i) {
-            next=nextarray(array,next);
-            printout(next,ofp);
-            array=change(array,next);
-        }
-        fclose(ofp);
-    }
-    else
-    {
-        printf("ERROR: Incorrect usage.\n");
-    }
-    return 0;
+    	int **array=create(),**next=create();
+    	int i;
+    	FILE *ofp;
+    	if( argc==2 )
+    	{
+        	ofp=fopen("wireworld_out.txt","wt");
+        	array=readin(array,argv[1]);
+        	for (i = 0; i < GENERATION; ++i) 
+		{
+            		next=nextarray(array,next);
+            		printout(next,ofp);
+            		array=change(array,next);
+        	}
+        	fclose(ofp);
+    	}
+    	else
+    	{
+        	printf("ERROR: Incorrect usage.\n");
+    	}
+    	return 0;
 }
 
 int **create()
 {
-    int i;
-    int **array=(int**)malloc(sizeof(int*)*LENGTH);
-    for(i=0;i<LENGTH;i++)
-    {
-        *(array+i)=(int*)malloc(sizeof(int)*LENGTH);
-    }
-    return array;
+    	int i;
+    	int **array=(int**)malloc(sizeof(int*)*LENGTH);
+    	for(i=0;i<LENGTH;i++)
+    	{
+        	*(array+i)=(int*)malloc(sizeof(int)*LENGTH);
+    	}
+    	return array;
 }
 
 int **readin(int **array,char *argv)
 {
-    FILE *fp;
-    int arrayx,arrayy;
-    char temp[LENGTH+2];
-    fp=fopen(argv,"r");
-    for(arrayx=0;arrayx<LENGTH;arrayx++)
-    {
-        fgets(temp,LENGTH+2,fp);
-        for(arrayy=0;arrayy<LENGTH;arrayy++)
-        {
-            *(*(array+arrayx)+arrayy)=temp[arrayy];
-        }
-        printf("\n");
-    }
-    fclose(fp);
-    return array;
+    	FILE *fp;
+    	int arrayx,arrayy;
+    	char temp[LENGTH+LINEBREAKLENTH];
+    	fp=fopen(argv,"r");
+    	for(arrayx=0;arrayx<LENGTH;arrayx++)
+    	{
+        	fgets(temp,LENGTH+LINEBREAKLENTH,fp);
+        	for(arrayy=0;arrayy<LENGTH;arrayy++)
+        	{
+            		*(*(array+arrayx)+arrayy)=temp[arrayy];
+        	}
+    	}
+    	fclose(fp);
+    	return array;
 }
 
 void printout(int **next,FILE *ofp)
 {
-    int nextx,nexty;
-    char elem;
-    for(nextx=0;nextx<LENGTH;nextx++)
-    {
-        for(nexty=0;nexty<LENGTH;nexty++) {
-            elem=*(*(next+nextx)+nexty);
-            fprintf(ofp,"%c",elem);
-        }
-        fprintf(ofp,"\n");
-    }
+    	int nextx,nexty;
+    	char elem;
+    	for(nextx=0;nextx<LENGTH;nextx++)
+    	{
+        	for(nexty=0;nexty<LENGTH;nexty++) 
+		{
+            		elem=*(*(next+nextx)+nexty);
+            		fprintf(ofp,"%c",elem);
+        	}
+        	fprintf(ofp,"\n");
+    	}
 }
 
 int **nextarray(int **array,int **next)
 {
-    int arrayx,arrayy;
-    for(arrayx=0;arrayx<LENGTH;arrayx++)
-    {
-        for(arrayy=0;arrayy<LENGTH;arrayy++)
-        {
-            *(*(next+arrayx)+arrayy)=nextelem(array,arrayx,arrayy);
-        }
-    }
-    return next;
+    	int arrayx,arrayy;
+    	for(arrayx=0;arrayx<LENGTH;arrayx++)
+    	{
+        	for(arrayy=0;arrayy<LENGTH;arrayy++)
+        	{
+            		*(*(next+arrayx)+arrayy)=nextelem(array,arrayx,arrayy);
+        	}
+    	}
+    	return next;
 }
 
 int **change(int **array,int **next)
 {
-    int arrayx,arrayy;
-    for(arrayx=0;arrayx<LENGTH;arrayx++)
-    {
-        for(arrayy=0;arrayy<LENGTH;arrayy++)
-        {
-            *(*(array+arrayx)+arrayy)=*(*(next+arrayx)+arrayy);
-        }
-    }
-    return array;
+    	int arrayx,arrayy;
+    	for(arrayx=0;arrayx<LENGTH;arrayx++)
+    	{
+        	for(arrayy=0;arrayy<LENGTH;arrayy++)
+        	{
+            		*(*(array+arrayx)+arrayy)=*(*(next+arrayx)+arrayy);
+        	}
+    	}
+    	return array;
 }
 
 int nextelem(int **array,int arrayx,int arrayy)
 {
-    switch (*(*(array+arrayx)+arrayy)) {
-        case EMPTY:
-            return EMPTY;
-        case HEAD:
-            return TAIL;
-        case TAIL:
-            return COPPER;
-        case COPPER:
-            if (findhead(array,arrayx,arrayy))
-            {
-                return HEAD;
-            }else
-            {
-                return COPPER;
-            }
-        default:
-            return -1;
-    }
+    	switch (*(*(array+arrayx)+arrayy)) {
+        	case EMPTY:
+            		return EMPTY;
+        	case HEAD:
+            		return TAIL;
+        	case TAIL:
+            		return COPPER;
+        	case COPPER:
+            		if (findhead(array,arrayx,arrayy))
+            		{
+                		return HEAD;
+            		}else
+            		{
+                		return COPPER;
+            		}
+        		default:
+            			return -1;
+    	}
 }
 
 int findhead(int **array,int arrayx,int arrayy)
 {
-    int i,j,cnt=0;
-    for(i=arrayx-1;i<arrayx+2;i++)
-    {
-        for (j =arrayy-1;j<arrayy+2; j++)
-        {
-            if (!(i<0 || j<0 || i>=LENGTH || j>=LENGTH || (i==arrayx && j==arrayy)))
-            {
-                if(*(*(array+i)+j)==HEAD) cnt++;
-            }
-        }
-    }
-    if((cnt==1)||(cnt==2)) return 1;
-    else return 0;
+    	int i,j,cnt=0;
+    	for(i=arrayx-1;i<arrayx+2;i++)
+    	{
+        	for (j =arrayy-1;j<arrayy+2; j++)
+        	{
+            		if (!(i<0 || j<0 || i>=LENGTH || j>=LENGTH || (i==arrayx && j==arrayy)))
+            		{
+                		if(*(*(array+i)+j)==HEAD) cnt++;
+            		}
+        	}
+    	}
+    	if((cnt==1)||(cnt==2)) return 1;
+    	else return 0;
 }
