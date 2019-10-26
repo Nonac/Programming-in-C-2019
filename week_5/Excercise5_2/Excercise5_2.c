c19acfe788212662b447198c26b1a49459edc359
nonac <nonacyang@gmail.com>
2019-10-26 16:03:25 +0100

    modified:   week_5/Excercise5_2/Excercise5_2.c

--- week_5/Excercise5_2/Excercise5_2.c
+++ week_5/Excercise5_2/Excercise5_2.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define ALPHABET 26
 #define LONGESTWORD 45
 
 typedef struct Node{
     unsigned long product;
+    int cnt;
     char **anagrams;
     struct Node *next;
 }Node;
 
 int *createprime(int *prime);
 Node *create();
-void search(Node *p, unsigned long mumultiply,char *wordp);
+void search(Node *head, unsigned long mumultiply,char *wordp);
+void printelem(Node *head);
 
 int main()
 {
@@ -19,14 +22,15 @@ int main()
     FILE *fp;
     char word[LONGESTWORD];
     int i;
-    unsigned long multiply=1;
-    Node *p;
+    unsigned long multiply;
+    Node *head;
 
     char *wordp=word;
     prime=createprime(prime);
-    p=create();
-    fp=fopen("eng_370k_shuffle.txt","r");
+    head=create();
+    fp=fopen("D:\\Coding Practise\\Clion_practise\\eng_370k_shuffle.txt","r");
     do{
+        multiply=1;
         for(i=0;i<LONGESTWORD;i++)
         {
             word[i]=EOF;
@@ -36,8 +40,10 @@ int main()
         {
             multiply*=prime[word[i]-'a'];
         }
-        search(p,multiply,wordp);
+        search(head,multiply,wordp);
     }while(word[0]!=EOF);
+    printelem(head);
+    return 0;
 }
 
 Node *create()
@@ -45,7 +51,9 @@ Node *create()
     Node *head;
     head=(Node *)malloc(sizeof(Node));
     head->product=1;
+    head->cnt=0;
     head->anagrams=(char **)malloc(sizeof(char*));
+    *(head->anagrams)=(char *)malloc(sizeof(char));
     head->next=NULL;
     return head;
 }
@@ -69,8 +77,48 @@ int *createprime(int *prime)
     return prime;
 }
 
-void search(Node *p, unsigned long mumultiply,char *wordp)
+void search(Node *head, unsigned long multiply,char *wordp)
 {
-
+    Node *p,*next;
+    int flag=0,i;
+    for(p=head;p->next!=NULL;p=p->next)
+    {
+        if(p->product==multiply)
+        {
+            p->cnt++;
+            p->anagrams=(char **)realloc(p->anagrams,p->cnt* sizeof(char *));
+            *(p->anagrams)=(char *)realloc(*(p->anagrams), strlen(wordp)+strlen(*(p->anagrams)));
+            *(p->anagrams+p->cnt-1)=wordp;
+            flag=1;
+        }
+    }
+    if((p->next==NULL)&&(flag==0))
+    {
+        next=create();
+        p->next=next;
+        p=p->next;
+        p->cnt++;
+        p->anagrams=(char **)realloc(p->anagrams,p->cnt* sizeof(char *));
+        *(p->anagrams)=(char *)realloc(*(p->anagrams), strlen(wordp)+strlen(*(p->anagrams)));
+        *(p->anagrams+p->cnt-1)=wordp;
+    }
 }
 
+void printelem(Node *head)
+{
+    Node *p;
+    int i,len;
+    for(p=head;p->next!=NULL;p=p->next)
+    {
+        printf("%d ",p->cnt);
+        for(i=0;i<p->cnt;i++)
+        {
+            for(len=0;len<strlen(*(p->anagrams+i));len++)
+            {
+                printf("%c",*(*(p->anagrams+i)+len));
+            }
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
