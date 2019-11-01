62dd74a54d3c671c564f7e212df145b06604076f
nonac <nonacyang@gmail.com>
2019-11-01 14:44:17 +0000

    modified:   week_6/Excercise6_5.c

--- week_6/Excercise6_5.c
+++ week_6/Excercise6_5.c
@@ -7,16 +7,19 @@ typedef struct node
     struct Node *next;
 }Node;
 
-void calprime(Node *head,char *argv);
+Node *calprime(char *argv);
 Node * create();
 Node *createprime(int temp);
+Node *insert(Node *head,int cnt);
+Node *findprime(Node *headprime, int temp);
+void printlink(char *argv,Node *head);
 
 int main(int argc, char **argv)
 {
     if(argc==2)
     {
-        Node *head=create();
-        calprime(head,argv[1]);
+        Node *head=calprime(argv[1]);
+        printlink(argv[1],head);
     } else{
         printf("Error.\n");
     }
@@ -31,21 +34,84 @@ Node *create()
 
 Node *createprime(int temp)
 {
-    int i,;
-    for(i=2;)
+    int i,cnt=2,flag=0;
+    Node *head=create();
+    while(cnt<=temp)
     {
-        
+        for (i = 2;  i<cnt ; i++)
+        {
+            if(cnt%i==0)
+            {
+                flag=1;
+            }
+        }
+        if (flag==0&&i==cnt)
+        {
+            head=insert(head,cnt);
+        }
+        cnt++;
+        flag=0;
     }
     return head;
 }
 
+Node *insert(Node *head,int cnt)
+{
+    Node *p=head,*temp=create();
+    while (p->next!=NULL)
+    {
+        p=p->next;
+    }
+    temp->prime=cnt;
+    p->next=temp;
+    temp->next=NULL;
+    return head;
+}
 
-void calprime(Node *head,char *argv)
+
+Node *calprime(char *argv)
 {
     int temp=atoi(argv);
     Node *headprime=createprime(temp);
-    while(temp%)
+    Node *p=headprime;
+    Node *head=create();
+    Node *restemp=create();
+    Node *current=head;
+    while(temp>1)
     {
+        p=findprime(p,temp);
+        restemp->prime=p->prime;
+        current->next=restemp;
+        current=current->next;
+        temp/=p->prime;
+    }
+    return head;
+}
 
+Node *findprime(Node *p, int temp)
+{
+    while (temp%p->prime!=0)
+    {
+        p=p->next;
+    }
+    return p;
+}
+
+void printlink(char *argv,Node *head)
+{
+    int cnt=0,temp=0;
+    Node *p=head;
+    printf("%d = ",atoi(argv));
+    temp=head->prime;
+    while (p->next!=NULL)
+    {
+        while(temp==p->prime)
+        {
+           p=p->next;
+           cnt++;
+        }
+        printf("%d^%d ",temp,cnt);
+        temp=p->prime;
+        cnt=0;
     }
 }
